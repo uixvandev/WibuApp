8fe9230119daf3c65bbdbc4997a2bf6353126947
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-07-18 22:46:57 +0700

    Initial Commit

---
+++ Wibu App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wibu App
+//
+//  Created by irfan wahendra on 18/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wibu App/Wibu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wibu_AppApp.swift
+//  Wibu App
+//
+//  Created by irfan wahendra on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Wibu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
